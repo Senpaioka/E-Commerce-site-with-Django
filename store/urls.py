10b0a45b91b48529d1e413e2cc048d696e7c570d
Abdul Mannan <abdul.gameprofile01@gmail.com>
2024-11-16 23:02:21 +0600

    Paginator & Search

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('store_page/', views.store_page_view, name='store_page'),
     path('store_page/<slug:category_slug>/', views.store_page_view, name='product_by_category'),
     path('store_page/<slug:category_slug>/<slug:selected_product_slug>/', views.product_details_page, name='product_details'),
+    path('search/', views.search_functionality_view, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,6 +3,8 @@ from store.models import Product
 from category.models import Categories
 from carts.models import CartItem
 from utils.user_session import _user_session_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -16,15 +18,27 @@ def store_page_view(request, category_slug=None):
 
     if category_slug != None:
         selected_category = get_object_or_404(Categories, slug=category_slug)
-        all_product = Product.objects.filter(category=selected_category, is_available=True)
+        all_product = Product.objects.filter(category=selected_category, is_available=True).order_by('id')
+        
+        # paginator functionality
+        paginator = Paginator(all_product, 6)
+        page = request.GET.get('page')
+        set_page = paginator.get_page(page)
+
         product_count = all_product.count()
 
     else:
-        all_product = Product.objects.all().filter(is_available = True)
+        all_product = Product.objects.all().filter(is_available = True).order_by('id')
+        
+        # paginator functionality
+        paginator = Paginator(all_product, 6)
+        page = request.GET.get('page')
+        set_page = paginator.get_page(page)
+
         product_count = all_product.count()
 
     context = {
-        'products': all_product,
+        'products': set_page,
         'count': product_count,
     }
 
@@ -52,6 +66,29 @@ def product_details_page(request, category_slug, selected_product_slug):
     context = {
         'product_info' : single_product,
         'is_cart_available': cart_empty_or_not,
+    }
+
+    return render(request, html_template_name, context)
+
+
+
+
+
+
+
+def search_functionality_view(request):
+
+    html_template_name = 'store/store.html'
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+
+        if keyword is not " ":
+            searched_product = Product.objects.order_by('-created_date').filter(Q(product_description__icontains=keyword) | Q(product_name__icontains=keyword))
+            found_product = searched_product.count()
+    context = {
+        'products' : searched_product,
+        'count': found_product,
     }
 
     return render(request, html_template_name, context)
